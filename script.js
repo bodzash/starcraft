50b723a79e47a80178d2757e3f76489cd3c7a6c8
bodzash <98101027+bodzash@users.noreply.github.com>
2022-04-19 23:23:21 +0200

    FeelsOkayMan

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ class Unit {
         this.spd = 0
         this.state = "idle" //idle-move-attack-attackmove-channel
         this.waypoints = []
-        this.dest = {x: null, y: null} //move
+        this.dest = null // {x: 1, y: 2,}
         this.target = null //id number maybe?
 
         this.unitName = "Default"
         this.unitSize = 32 //px
         this.unitModel = ""
-        this.unitHealth = 100
+        this.unitHealth = 50
         this.unitSpeed = 4
         this.unitArmor = 1 //1-2-3 L-M-H (0-None)
         this.unitPen = {L: 1, M: 1, H: 1} //1-5 (1-worst 5-best)
@@ -34,51 +34,61 @@ class Unit {
         this.x += this.spd*Math.cos(3.14/180*this.dir)
         this.y -= this.spd*Math.sin(3.14/180*this.dir)
 
-        //Snap to grid (for pathfind)
-        //Math.floor(mouseX/32)*32
-        //Math.floor(mouseY/32)*32 
+        // State Controller break it up :)
+        /*switch(state) {
+            case "idle":
+            break
+        }*/
 
-        // Selection Check
-        if (mouse !== null) {
-            let ap = pointDirection(this.x, this.y, mouseX, mouseY)
-            this.dir = ap
-            this.spd = this.unitSpeed
-            let x1 = Math.min(mouse.x, mouseX)
-            let y1 = Math.min(mouse.y, mouseY)
-            let x2 = Math.max(mouse.x, mouseX)
-            let y2 = Math.max(mouse.y, mouseY)
-            if (x2 >= this.x - this.w/2 && x1 <= this.x + this.w/2 &&
-                y2 >= this.y - this.w/2 && y1 <= this.y + this.w/2) {
-                    this.isSelected = true
+        // Move to point
+        if (this.dest !== null) {
+            if (pointDistance(this.x, this.y, this.dest.x, this.dest.y) >= this.unitSpeed) {
+                this.dir = pointDirection(this.x, this.y, this.dest.x, this.dest.y)
+                this.spd = this.unitSpeed
+            } else {
+                this.x = this.dest.x
+                this.y = this.dest.y
+                this.dest = null
+                this.state = "idle"
+                this.spd = 0
+            }
+        }
+
+        // Mouse Left Released Event
+        if (mouseLeftReleased) {
+            if (insideMouseCmdr(this)) {
+                this.isSelected = true
+                selectedUnits.push(this)
             } else {
                 this.isSelected = false
             }
-        } else {
-            this.spd =  0
+            
         }
 
         // Collision With Other Unit
         unitArray.forEach(item=> {
             if (this.id !== item.id) {
                 //let other = item.id
-                if (checkCollInst(item, this) && this.spd === 0) {
+                if (checkCollInst(item, this) && this.spd === 0 && item.spd === 0) {
                     let ang = pointDirection(this.x, this.y, item.x, item.y) - 180
-                    this.x += 3 * Math.cos(3.14 / 180 * ang)
-                    this.y -= 3 * Math.sin(3.14 / 180 * ang)
+                    this.x += 2 * Math.cos(3.14 / 180 * ang)
+                    this.y -= 2 * Math.sin(3.14 / 180 * ang)
                 }
             }
         })
 
-        this.draw() //Draw
+        // Draw
+        this.draw()
     }
     draw() {
-        this.selected ? ctx.fillStyle = "green" : ctx.fillStyle = "red"
+        this.isSelected ? ctx.fillStyle = "green" : ctx.fillStyle = "red"
         ctx.fillRect(this.x-this.w/2, this.y-this.w/2, this.w, this.w)
     }
 }
 
 class MouseCommander {
     constructor(x, y) {
+        selectedUnits = []
         this.x = x
         this.y = y
     }
@@ -105,12 +115,12 @@ let wallArray = []
 // Constant variables
 let mouseX = 0
 let mouseY = 0
-let mousePressed = false
-let mouseReleased = false
-let mouseHold = false
+let mouseLeftPressed = false
+let mouseLeftReleased = false
+let mouseLeftHold = false
 
 // Global Instances
-let mouse = null
+let mouseCmdr = null
 let selectedUnits = []
 
 // Pathfinder
@@ -133,19 +143,21 @@ unitArray.push(new Unit(100, 232, 16, 0))
 unitArray.push(new Unit(164, 200, 16, 0))
 unitArray.push(new Unit(164, 232, 16, 0))
 
-// Loop the game
+// ########### GAME LOOP ###########
 function update() {
     requestAnimationFrame(update)
     ctx.clearRect(0, 0, canvas.width, canvas.height) //cls
-    
+
     unitArray.forEach(item=> item.step())
-    mouse !== null && mouse.step()
+    mouseCmdr !== null && mouseCmdr.step()
 }
 update()
 
 
 // ########### Functions ###########
-function checkCollInst(obj1, obj2) { //check collision between 2 instances
+
+// Checks collision between 2 instances
+function checkCollInst(obj1, obj2) {
     if (
         obj1.x + obj1.w/2 >= obj2.x - obj2.w/2 &&
         obj1.x - obj1.w/2 <= obj2.x + obj2.w/2 &&
@@ -155,23 +167,70 @@ function checkCollInst(obj1, obj2) { //check collision between 2 instances
     {return true} else {return false}
 }
 
+// Returns angle between two points
 function pointDirection(x1, y1, x2, y2) { //
     let rad = Math.atan2(y1 - y2, x1 - x2) * ( 180 / 3.14 )
     return Math.abs(rad - 180)
 }
 
-//Move Unit towards an angle
+// Distance between two points
+function pointDistance(x1, y1, x2, y2) {
+    let a = x1 - x2
+    let b = y1 - y2
+    return Math.sqrt(a * a + b * b)
+}
+
+// Move Unit towards an angle
 function motionAdd(dir, spd) {
     hspeed += spd*cos(pi/180*dir)
     vspeed -= spd*sin(pi/180*dir)
 }
 
-// Evenet Listeners
-canvas.onmousemove = (e)=> {
-    mouseX = e.clientX
-    mouseY = e.clientY
+// Checks if instance inside mouseCmdr selection
+function insideMouseCmdr(obj) {
+
+    if (mouseCmdr === null) {return false}
+
+    let x1 = Math.min(mouseCmdr.x, mouseX)
+    let y1 = Math.min(mouseCmdr.y, mouseY)
+    let x2 = Math.max(mouseCmdr.x, mouseX)
+    let y2 = Math.max(mouseCmdr.y, mouseY)
+    
+    if (
+    x2 >= obj.x - obj.w/2 &&
+    x1 <= obj.x + obj.w/2 &&
+    y2 >= obj.y - obj.w/2 &&
+    y1 <= obj.y + obj.w/2) {
+        return true
+    } else {
+        return false
+    }
 }
-canvas.onmousedown = (e)=> {
-    mouse = new MouseCommander(e.clientX,e.clientY)
+
+
+// ########### Evenet Listeners ###########
+onmousemove = ({ clientX, clientY })=> {
+    mouseX = clientX
+    mouseY = clientY
 }
-canvas.onmouseup = ()=> { mouse = null }
+onmousedown = (e)=> { // 0-left 2-right
+    mouseLeftPressed = true
+    mouseLeftHold = true
+    requestAnimationFrame(()=> {mouseLeftPressed=false}) // skip 1 frame
+    mouseCmdr = new MouseCommander(e.clientX,e.clientY)
+}
+onmouseup = (e)=> { // 0-left 2-right
+    mouseLeftReleased = true
+    mouseLeftHold = false
+    requestAnimationFrame(()=> {
+        mouseLeftReleased=false
+        mouseCmdr = null
+    })
+}
+
+onkeydown = ({ key })=> { //e.code or e.key
+    //console.log(selectedUnits)
+    selectedUnits.forEach(item=> {
+        item.dest = {x: Math.floor(mouseX/32)*32+16, y: Math.floor(mouseY/32)*32+16}
+    })
+}
